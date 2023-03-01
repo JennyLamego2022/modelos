00f799bf4c134820c7eb2aae792bd891313f1993
JennyLamego2022 <jennylamego@hotmail.com>
2023-03-01 08:39:34 -0300

    feat: Função buscar por cidade, funcionando no JavaScript

--- Blocos de Carnaval/assets/js/main.js
+++ Blocos de Carnaval/assets/js/main.js
@@ -1,4 +1,30 @@
-const filterElement = document.querySelector('header input')
+// const filterElement = document.querySelector('header input')
+// const cards = document.querySelectorAll('.cards li')
+
+// filterElement.addEventListener('input', filterTitle)
+
+// function filterTitle(){
+//    if(filterElement.value != ''){
+//     for(let card of cards){
+//         let title = card.querySelector('h3')
+//         title = title.textContent.toLowerCase()
+//         console.log(title)
+
+//         let filterText = filterElement.value.toLowerCase()
+//         if(!title.includes(filterText)){
+//             card.style.display = 'none'
+//             console.log(card)
+//         }else{
+//             card.style.display = 'block'
+//         }
+//     }
+//    }else{
+//     for(let card of cards){
+//         card.style.display = 'block'
+//     }
+//    }
+// }
+const filterElement = document.querySelector('header select')
 const cards = document.querySelectorAll('.cards li')
 
 filterElement.addEventListener('input', filterTitle)
@@ -6,7 +32,7 @@ filterElement.addEventListener('input', filterTitle)
 function filterTitle(){
    if(filterElement.value != ''){
     for(let card of cards){
-        let title = card.querySelector('h3')
+        let title = card.querySelector('.location')
         title = title.textContent.toLowerCase()
         console.log(title)
 
@@ -24,3 +50,9 @@ function filterTitle(){
     }
    }
 }
+
+
+
+// function rio(){
+//     alert('clicado')
+// }
